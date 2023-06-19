ca2f03704f58ed8e60398f48c5fe23a59c78fd57
Guilherme Vieira Barbosa <127813184+Guil3r@users.noreply.github.com>
2023-06-18 21:50:28 -0300

    add rendimento corte

---
+++ rendimento_tecido.js
@@ -0,0 +1,19 @@
+function rendimento(){
+    let tecido = document.getElementById('tecido')
+    let cmatriz = document.getElementById('cmatriz')
+    let quantidade = document.getElementById('qtn')
+
+    if ( tecido.value == 0 || cmatriz.value == 0 || quantidade.value == 0) {
+        window.alert('[ERRO] Faltam dados !')
+    } else {
+        let metros = Number(tecido.value)
+        let comp = Number(cmatriz.value)
+        let qt = Number(quantidade.value)
+
+        let divisao = ( metros / comp ) -1
+        let res = divisao * qt
+
+        resultado.innerHTML = 'Total:  ' + Math.trunc(`${res}`) + ' peças aproximadamente'
+        
+    }
+}
